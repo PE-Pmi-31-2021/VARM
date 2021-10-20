0a6e8253c1ffd4c4f5244bd120482a5a2df12c02
Andriy Vdovitsyn <007andriyua@gmail.com>
2021-10-21 00:10:57 +0300

    Add project files.

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LNU_VARM_games
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            Binding binding = new Binding();
+            binding.ElementName = "MainFrame1";
+            binding.Path = new PropertyPath("Source");
+
+            //MainFrame.SetBinding(Frame.SourceProperty, binding);
+
+
+
+            Game1 g = new Game1();
+            this.NavigationService.Navigate(g);
+
+
+            //MainFrame.Source = new Uri("pack://application:,,,/VARM games TEST;component/Game1.xaml");
+
+            //Game1 g = new Game1();
+            //this.Content = g;
+            //MainFrame.Source = new Uri("Game1.xaml");
+            //GameWindow g_win = new GameWindow();
+            //g_win.Owner = this;
+            //g_win.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            //g_win.Show();
+
+            //g_win.Owner = null;
+            //this.Close();
+        }
+
+    }
+}
